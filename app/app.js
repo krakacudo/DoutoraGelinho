ffbdf5e85c261b142ce9093fe710080ba40e2407
kraka <lkraka2000@gmail.com>
2023-12-16 18:54:43 +0000

    Homepage for Xoldiers

---
+++ app/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (_req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.listen(port, () => {
+    console.log(`Server listening in port ${port}`);
+})
