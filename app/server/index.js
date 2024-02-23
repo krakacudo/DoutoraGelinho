79038387f8cd66254d7b033cfea859a9993ffbc3
kraka <lkraka2000@gmail.com>
2024-02-23 14:23:29 +0000

    change path to index.html

--- app/server/index.js
+++ app/server/index.js
@@ -7,7 +7,7 @@ const port = 3000;
 app.use(express.static('./public'));
 
 app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
 app.listen(port, () => {
