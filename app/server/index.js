66575576f0b7c088583557776340382350d06877
kraka <lkraka2000@gmail.com>
2024-02-24 16:07:55 +0000

    build for production test

--- app/server/index.js
+++ app/server/index.js
@@ -4,10 +4,10 @@ const app = express();
 
 const port = 3000;
 
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+app.get('/', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
 })
 
 app.listen(port, () => {
