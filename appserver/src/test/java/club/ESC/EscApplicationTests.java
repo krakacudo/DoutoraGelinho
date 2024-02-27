60f41fa3920841d074dbaa88f027f73dbaa5a8d2
kraka <lkraka2000@gmail.com>
2024-02-27 08:33:53 +0000

    add java server

---
+++ appserver/src/test/java/club/ESC/EscApplicationTests.java
@@ -0,0 +1,13 @@
+package club.ESC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
