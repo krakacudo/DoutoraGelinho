f2dad660ededf7b1c94acc18487c9f21721dea61
kraka <lkraka2000@gmail.com>
2024-03-17 17:25:37 +0000

    dev: make it easy

--- app/client/client.module.ts
+++ app/client/client.module.ts
@@ -6,10 +6,14 @@ import { AppRoutingModule } from "./client-routing.module";
 import { PlayerComponent } from "./players/player.component";
 import { HttpClientModule } from "@angular/common/http"
 import { PlayerService } from "./players/player.service";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatDividerModule } from '@angular/material/divider';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
-    imports: [BrowserModule, AppRoutingModule, CommonModule, HttpClientModule],
-    providers: [PlayerService],
+    imports: [BrowserModule, AppRoutingModule, CommonModule, HttpClientModule, MatButtonModule, MatIconModule, MatDividerModule],
+    providers: [PlayerService, provideAnimationsAsync()],
     declarations: [ClientComponent, PlayerComponent],
     bootstrap: [ClientComponent]
 })
